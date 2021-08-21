adea6b03b1537a2b776b4835f47dbf3a9129d81e
Paweł Wójcik <pawel.jakub.wojcik@gmail.com>
2021-08-22 00:17:49 +0200

    initialize with tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        fontFamily: {
+            sans: ['Graphik', 'sans-serif'],
+            serif: ['Merriweather', 'serif'],
+        },
+        extend: {
+            spacing: {
+                128: '32rem',
+                144: '36rem',
+            },
+            borderRadius: {
+                '4xl': '2rem',
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
